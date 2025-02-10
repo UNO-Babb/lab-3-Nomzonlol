6fec7807b0c21ad8497bc556d0df02d0d6959a32
Nomzonlol <snomaan070@gmail.com>
2025-02-10 05:34:26 +0000

    Done with ApproxPi.

--- ApproxPi.py
+++ ApproxPi.py
@@ -1,26 +1,36 @@
 #ApproxPi.py
-#Name:
-#Date:
-#Assignment:
+#Name:Nomaan Ahmed 
+#Date:2/9/25
+#Assignment:Lab 3 
 import math
 import time
 
+def calculate_pi(precision):
+    pi = 0
+    for i in range(precision):
+        pi += ((-1) ** i) / (2 * i + 1)
+    return pi * 4
 
 #PLEASE NOTE - This is an optional, extra credit portion of the lab.
 
 def main():
+  target_pi = 3.14159
   realPi = math.pi
 
   #ask user for decimal percision (up to 10)
+  precision = int(input("Enter the number of terms for the approximation (e.g., 10000): "))
 
   start = time.time()
   #calculate pi using the approximation technique
+  pi_approximation = calculate_pi(precision)
   #Loop until the level of percision is reached
-
+  
   end = time.time()
 
   elapsedTime = end - start
-  print(elapsedTime)
-
+  print(f"Approximated value of pi: {pi_approximation}")
+  print(f"Actual value of pi: {realPi}")
+  print(f"Elapsed time: {elapsedTime} seconds")
+  
 if __name__ == '__main__':
   main()
